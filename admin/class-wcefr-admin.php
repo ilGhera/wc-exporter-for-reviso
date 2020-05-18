d0ef85854cdb06ea7fce3b6ebe48155ae08c83b6
ilGhera <andrea@ilghera.com>
2020-05-18 10:01:48 +0200

    Rimosso iframe da sidebar

--- admin/class-wcefr-admin.php
+++ admin/class-wcefr-admin.php
@@ -169,9 +169,7 @@ class WCEFR_Admin {
 
 			echo '</div>';
 
-			echo '<div class="wrap-right">';
-				echo '<!-- <iframe width="300" height="900" scrolling="no" src="https://www.ilghera.com/images/wcefr-premium-iframe.html"></iframe> -->';
-			echo '</div>';
+			echo '<div class="wrap-right"></div>';
 
 			echo '<div class="clear"></div>';
 

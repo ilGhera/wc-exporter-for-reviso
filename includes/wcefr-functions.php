161ec158b9380474d857f7f51def121b1ec4c902
ilGhera <andrea@ilghera.com>
2020-05-18 07:53:24 +0200

    Rimosso richiamo plugin update checker Funzione go_premium CSS go_premium

--- includes/wcefr-functions.php
+++ includes/wcefr-functions.php
@@ -8,83 +8,26 @@
  */
 
 /**
- * Update checker
+ * Go premium button
  */
-require( WCEFR_DIR . 'plugin-update-checker/plugin-update-checker.php' );
+function wcefr_go_premium() {
 
-$wcefr_update_checker = Puc_v4_Factory::buildUpdateChecker(
-	'https://www.ilghera.com/wp-update-server-2/?action=get_metadata&slug=wc-exporter-for-reviso-premium',
-	WCEFR_FILE,
-	'wc-exporter-for-reviso-premium'
-);
+	$title = __( 'This is a premium functionality, click here for more information', 'crm-in-cloud-for-wc' );
+	$output = '<span class="crmfwc label label-warning premium">';
+		$output .= '<a href="https://www.ilghera.com/product/woocommerce-exporter-for-reviso-premium" target="_blank" title="' . esc_attr( $title ) . '">Premium</a>';
+	$output .= '</span>';
 
-
-/**
- * Secure update check with the Premium Key
- *
- * @param  array $query_args the default args.
- * @return array            the updated args
- */
-function wcefr_secure_update_check( $query_args ) {
-
-	$key = base64_encode( get_option( 'wcefr-premium-key' ) );
-
-	if ( $key ) {
-
-		$query_args['premium-key'] = $key;
-
-	}
-
-	return $query_args;
-
-}
-$wcefr_update_checker->addQueryArgFilter( 'wcefr_secure_update_check' );
-
-
-/**
- * Plugin update message
- *
- * @param  array $plugin_data plugin information.
- * @param  array $response    available plugin update information.
- */
-function wcefr_update_message( $plugin_data, $response ) {
-
-	$key = get_option( 'wcefr-premium-key' );
-
-	$message = null;
-
-	if ( ! $key ) {
-
-		$message = 'A <b>Premium Key</b> is required for keeping this plugin up to date. Please, add yours in the <a href="' . WCEFR_SETTINGS . '">options page</a> or click <a href="https://www.ilghera.com/product/wc-exporter-for-reviso-premium/" target="_blank">here</a> for prices and details.';
-
-	} else {
-
-		$decoded_key = explode( '|', base64_decode( $key ) );
-		$bought_date = date( 'd-m-Y', strtotime( $decoded_key[1] ) );
-		$limit = strtotime( $bought_date . ' + 365 day' );
-		$now = strtotime( 'today' );
-
-		if ( $limit < $now ) {
-
-			$message = 'It seems like your <strong>Premium Key</strong> is expired. Please, click <a href="https://www.ilghera.com/product/wc-exporter-for-reviso-premium/" target="_blank">here</a> for prices and details.';
-
-		} elseif ( '7082' !== $decoded_key[2] ) {
-
-			$message = 'It seems like your <strong>Premium Key</strong> is not valid. Please, click <a href="https://www.ilghera.com/product/wc-exporter-for-reviso-premium/" target="_blank">here</a> for prices and details.';
-
-		}
-
-	}
-
-	$allowed_tags = array(
-		'strong' => array(),
-		'a'      => array(
-			'href'   => array(),
+	$allowed = array(
+		'span' => array(
+			'class' => array(),
+		),
+		'a'    => array(
 			'target' => array(),
+			'title'  => array(),
+			'href'   => array(),
 		),
 	);
 
-	echo ( $message ) ? '<br><span class="wcefr-alert">' . wp_kses( $message, $allowed_tags ) . '</span>' : '';
+	echo wp_kses( $output, $allowed );
 
 }
-add_action( 'in_plugin_update_message-' . WCEFR_DIR_NAME . '/wc-exporter-for-reviso.php', 'wcefr_update_message', 10, 2 );

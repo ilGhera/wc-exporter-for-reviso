065722dcfc6024e147ed484286885e8973d792fc
ilGhera <andrea@ilghera.com>
2021-01-18 16:03:13 +0100

    File js in cartella submodule Richiamo classe checkout field in main file

--- wc-exporter-for-reviso.php
+++ wc-exporter-for-reviso.php
@@ -45,9 +45,8 @@ function load_wc_exporter_for_reviso() {
 	require_once( WCEFR_INCLUDES . 'class-wcefr-settings.php' );
 	require_once( WCEFR_INCLUDES . 'class-wcefr-users.php' );
 	require_once( WCEFR_INCLUDES . 'class-wcefr-products.php' );
-	require_once( WCEFR_INCLUDES . 'class-wcefr-checkout-fields.php' );
 	require_once( WCEFR_INCLUDES . 'class-wcefr-orders.php' );
 	require_once( WCEFR_INCLUDES . 'wcefr-invoice.php' );
-
+	require_once( WCEFR_INCLUDES . 'wc-checkout-fields/class-wcefr-checkout-fields.php' );
 }
 add_action( 'after_setup_theme', 'load_wc_exporter_for_reviso', 1 );
